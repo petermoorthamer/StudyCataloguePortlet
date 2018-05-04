9aead88acbc4e15c2417c7feb218939e898aa1f2
Peter Moorthamer <peter@moorthamer.be>
2018-05-04 14:13:17 +0200

    Configuration changes for deployment on AWS

--- src/main/java/com/jnj/honeur/portlet/PortletConfiguration.java
+++ src/main/java/com/jnj/honeur/portlet/PortletConfiguration.java
@@ -6,6 +6,10 @@ import com.liferay.portal.kernel.util.PortalClassLoaderUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Helper class for loading the portlet configuration properties
  * @author Peter Moorthamer
@@ -15,6 +19,7 @@ public class PortletConfiguration {
     private static final Logger LOGGER = LoggerFactory.getLogger(PortletConfiguration.class);
 
     private static Configuration configuration;
+    private static Properties properties;
 
     private static Configuration getConfiguration() {
         if(configuration == null) {
@@ -26,11 +31,29 @@ public class PortletConfiguration {
         return configuration;
     }
 
+    private static Properties getProperties() {
+        if(properties == null) {
+            try {
+                InputStream input = PortletConfiguration.class.getClassLoader().getResourceAsStream("portlet.properties");
+                properties = new Properties();
+                properties.load(input);
+            } catch (IOException e) {
+                LOGGER.warn("Unable to read from properties file portlet.properties");
+                properties = null;
+            }
+        }
+        return properties;
+    }
+
     private static String getProperty(String propertyName) {
         String propertyValue = System.getenv(propertyName);
         LOGGER.info(String.format("Environment variable %s = %s", propertyName, propertyValue));
         if(propertyValue == null && getConfiguration() != null) {
             propertyValue = getConfiguration().get(propertyName);
+            LOGGER.info(String.format("Portlet property %s = %s", propertyName, propertyValue));
+        }
+        if(propertyValue == null && getProperties() != null) {
+            propertyValue = getProperties().getProperty(propertyName);
             LOGGER.info(String.format("Portlet property %s = %s", propertyName, propertyValue));
         }
         if(propertyValue == null) {
